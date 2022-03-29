adbbe65bcd675350d3822d70565d40806ce1ca3b
JeanFrangeul <frangeuljean@gmail.com>
2022-03-29 19:13:22 +0200

    store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import './App.css';
+import Meal from './Meal.js';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  const [meal, setMeal] = useState(null);
+
+  useEffect(() => {
+      (async function () {
+
+          axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+          .then((response) => {
+              setMeal(response.data.meals[0]);
+          })
+
+          /* ici on utilise FETCH()
+          const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+          const response = await fetch(url);
+
+          const mealsFromApi = await response.json();
+
+          meal = mealsFromApi.meals[0];
+          */
+          
+      })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -8,8 +35,9 @@ function App() {
           Hello World !!
         </p>
       </header>
+      <Meal meal={meal}/>
     </div>
   );
-}
+};
 
 export default App;
